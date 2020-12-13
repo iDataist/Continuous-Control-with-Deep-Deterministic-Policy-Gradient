37692e699ada7e7ce93b20fc03a31079dc97df52
iDataist <hwdgrmy@gmail.com>
2020-12-13 13:59:10 -0600

    Update ddpg_agent.py

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -13,7 +13,7 @@ BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor
+LR_ACTOR = 1e-3         # learning rate of the actor
 LR_CRITIC = 1e-3        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 
